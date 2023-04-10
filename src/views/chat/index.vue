6ef0b7f038be073e46ab8a9e21a6971f38d606c9
shier03 <90560523+shier03@users.noreply.github.com>
2023-04-10 22:06:37 +0800

    Update index.vue

--- src/views/chat/index.vue
+++ src/views/chat/index.vue
@@ -485,6 +485,15 @@ onUnmounted(() => {
             <div class="flex items-center justify-center mt-4 text-center text-neutral-300">
               <SvgIcon icon="ri:bubble-chart-fill" class="mr-2 text-3xl" />
               <span>Aha~</span>
+							              <div>免费提供学习和测试，切勿发布至国内平台或微信分享</div>
+              <div>支持上下文，支持开启关闭联网模式，支持保存会话</div>
+              <div>。</div>
+              <div>。</div>
+              <div>.</div>
+              <div>.</div>
+              <div>
+                <img src="http://43.138.242.195:82/f.gif" alt="kele">
+              </div>
             </div>
           </template>
           <template v-else>
